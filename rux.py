91b77318a485cbe2bafb4771ff226127fa4f8d6f
Roma <addresing88@gmail.com>
2025-02-16 14:30:20 +0200

    Update rux.py

--- rux.py
+++ rux.py
@@ -2,23 +2,58 @@ from PyQt5.QtWidgets import*
 
 import sys
 from PyQt5.QtWidgets import*
+from googletrans import Translator
+
 
 
 app = QApplication(sys.argv)
 window = QWidget()
+translator = Translator()
+
+
+text_to_translate = "Hello, how are you?"
+
+langs = {
+   "Англійська": "en",
+   "Українська": "uk",
+   "Японська": "ja",
+   "Азейбарджанська": "az",
+   "Казахстанська": "kk",
+   "Німецька": "de",
+   "Французька": "fr",
+   "Польська": "pl",
+   "Китайська": "zh-tw",
+}
 
 input_text = QTextEdit()
 autput_edit = QTextEdit()
-button = QPushButton( "-> <-" )
+button = QPushButton("-> <-")
 button2 = QPushButton("Перекласти")
 
 combo = QComboBox()
 combo.addItem('Англійська')
+combo.addItem('Японська')
+combo.addItem('Азейбарджанська')
+combo.addItem('Казахстанська')
+combo.addItem('Німецька')
+combo.addItem('Французька')
+combo.addItem('Польська')
+combo.addItem('Китайська')
+
 
 
 
 combo2 = QComboBox()
 combo2.addItem("Українська")
+combo2.addItem('Японська')
+combo2.addItem('Азейбарджанська')
+combo2.addItem('Казахстанська')
+combo2.addItem('Німецька')
+combo2.addItem('Французька')
+combo2.addItem('Польська')
+combo2.addItem('Китайська')
+
+
 
 main_line = QHBoxLayout()
 
@@ -35,6 +70,14 @@ main_line.addLayout(v_line)
 main_line.addWidget(button)
 main_line.addLayout(v_line2)
 main_line.addWidget(button2)
+
+def print_text():
+   a = input_text.toPlainText()
+   translated_text = translator.translate(a, src=langs[combo.currentText()], dest=langs[combo2.currentText()])
+   print(f"Original Text: {text_to_translate}")
+   print(f"Translated Text: {translated_text.text}")
+   autput_edit.setText(translated_text.text)
+button.clicked.connect(print_text)
 
 window.setLayout(main_line)
 window.show()
